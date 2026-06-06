ac143151c130273d1d8813cfb4f3e68d6b132b95
mathu <mathuvith35@gmail.com>
2026-06-06 17:19:07 +0530

    inventory  dashboard product category

--- frontend/src/routes/AdminRoutes.tsx
+++ frontend/src/routes/AdminRoutes.tsx
@@ -3,6 +3,7 @@ import DashboardPage from '../pages/admin/Dashboard/DashboardPage'
 import InventoryPage from '../pages/inventory/Dashboard/InventoryPage'
 import SettingsPage from '../pages/admin/settings/Settings'
 import ReportsPage from '../pages/admin/Reports/Reports'
+import AccountManagementPage from '../pages/admin/AccountManagement/AccountManagement'
 
 export default function AdminRoutes() {
   return (
@@ -11,6 +12,7 @@ export default function AdminRoutes() {
       <Route path="inventory" element={<InventoryPage />} />
       <Route path="settings" element={<SettingsPage />} />
       <Route path="reports" element={<ReportsPage />} />
+      <Route path="accounts" element={<AccountManagementPage />} />
     </Routes>
   )
 }
